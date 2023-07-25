35c5b4aff8c3041ae24d875d784ae4ec2430f90b
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-07-25 19:14:41 -0300

    add: banner y corrusel

---
+++ public/js/carouselSwiper.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
